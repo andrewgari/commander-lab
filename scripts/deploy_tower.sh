b96d6e03f74eedc285d1e4b2ac27e6266f3281b0
Andrew Gari <covadax.ag@gmail.com>
2026-09-01 15:24:31 -0400

    Tag and push image to Tower local docker registry

--- scripts/deploy_tower.sh
+++ scripts/deploy_tower.sh
@@ -12,9 +12,15 @@ echo "Building Docker image on Tower (${TOWER_HOST})..."
 ssh "${TOWER_HOST}" "rm -rf ${REMOTE_BUILD_DIR} && \
   git clone http://127.0.0.1:3004/CovaDax/commander-lab.git ${REMOTE_BUILD_DIR} && \
   cd ${REMOTE_BUILD_DIR} && \
-  docker build -t commander-lab:${IMAGE_TAG} -t commander-lab:latest ."
+  docker build \
+    -t commander-lab:${IMAGE_TAG} \
+    -t commander-lab:latest \
+    -t localhost:5000/commander-lab:${IMAGE_TAG} \
+    -t localhost:5000/commander-lab:latest . && \
+  docker push localhost:5000/commander-lab:${IMAGE_TAG} && \
+  docker push localhost:5000/commander-lab:latest"
 
 echo "Cleaning up temporary build artifacts..."
 ssh "${TOWER_HOST}" "rm -rf ${REMOTE_BUILD_DIR}"
 
-echo "Docker image commander-lab:${IMAGE_TAG} successfully built and available on Tower!"
+echo "Docker image commander-lab:${IMAGE_TAG} successfully built and pushed to Tower registry (localhost:5000/commander-lab)!"
